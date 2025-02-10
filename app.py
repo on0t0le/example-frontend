d0c05230532f8a7f3b11ca3ed60af36bb624ce10
anatolii.b <anatolii.b@opsworks.co>
2025-02-10 14:35:24 +0200

    Add user button

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 import os
 
@@ -14,6 +14,20 @@ def index():
     users = response.json()
 
     return render_template('index.html', users=users)
+
+@app.route('/add_user', methods=['POST'])
+def create_user():
+    # Get form data
+    name = request.form.get('name')
+    email = request.form.get('email')
+
+    requests.post(f"{API_URL}/users", json={
+        'name': name,
+        'email': email
+    })
+
+    # Redirect back to index page
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port=8080)
